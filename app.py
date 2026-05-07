6e8d09ba1b4ff3f45109d2e8965c82bd0efebe73
Suad-DevOps <suadsalaad2002@gmail.com>
2026-05-07 17:19:59 +0100

    Add Flask and Redis multi-container app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def home():
+    return 'Hello!'
+
+@app.route('/count')
+def count():
+    count = redis.incr('visits')
+    return f'Visit count: {count}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
