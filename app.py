d20e5880816ef38275df7c31a7eeb5983c75d86b
Suad-DevOps <suadsalaad2002@gmail.com>
2026-05-08 17:14:06 +0100

    Add screenshots

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis
 import os
 
@@ -7,12 +7,12 @@ redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=int(os.environ.ge
 
 @app.route('/')
 def home():
-    return 'Hello!'
+    return render_template('index.html')
 
 @app.route('/count')
 def count():
     count = redis.incr('visits')
-    return f'Visit count: {count}'
+    return render_template('index.html', count=count)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
